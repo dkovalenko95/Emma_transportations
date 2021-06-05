5d033c3232e2236cdfdb5272bbac13699142d21d
Dmytro <example@gmail.com>
2021-06-05 23:13:33 +0300

    business card relocated

--- dist/js/script.js
+++ dist/js/script.js
@@ -49,20 +49,3 @@ $(document).ready(function() {
         ]
     });
 });
-
-// Cards
-$('.toggle').click(function(e) {
-    e.preventDefault();
-
-  var $this = $(this);
-
-  if ($this.next().hasClass('show')) {
-      $this.next().removeClass('show');
-      $this.next().slideUp(350);
-  } else {
-      $this.parent().parent().find('li .inner').removeClass('show');
-      $this.parent().parent().find('li .inner').slideUp(350);
-      $this.next().toggleClass('show');
-      $this.next().slideToggle(350);
-  }
-});
